b3db2382e4944b0f7df9be6bf2b504494e1df05a
Almog ,Eden <edencaliber@gmail.com>
2025-12-06 13:25:49 +0200

    hello world first

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
+    print("Hello World")
     print("Welcome to Git Assignment")
     print("This is the clean version after resolving conflict")
 
 if __name__ == "__main__":
     main()
-
