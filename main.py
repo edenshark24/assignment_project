4ffa72d55bd30809ebd05f7b603a1e0a05bec927
Almog ,Eden <edencaliber@gmail.com>
2025-12-06 12:40:13 +0200

    malawach

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
 def main():
-    print("Welcome to Git Assignment")
-    print("Created by: shtrudel")
+    print("Welcome to malawach Assignment")
+    print("Created by: Eden")
     now = datetime.now()
     print("Current date and time:", now)
 
